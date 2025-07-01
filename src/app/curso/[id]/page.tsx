b57726b227c1693ac4133f9e22aa69162a25efa0
Jaime <148376774+jaimestegenfot@users.noreply.github.com>
2025-07-01 10:37:34 -0500

    g

--- src/app/curso/[id]/page.tsx
+++ src/app/curso/[id]/page.tsx
@@ -7,7 +7,7 @@ function getYouTubeEmbedUrl(url: string) {
   return match ? `https://www.youtube.com/embed/${match[1]}` : null;
 }
 
-export default function CursoDetallePage({ params }: { params: { id: string } }) {
+export default function CursoDetallePage({ params }: any) {
   const cursoId = parseInt(params.id);
   const curso = cursos.find(c => c.id === cursoId);
 
